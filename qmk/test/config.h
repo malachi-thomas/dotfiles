c8a2b99a27246b11de978fc214125dd983914f63
malachi557 <thomasmalachi18@gmail>
2020-08-25 16:39:38 +0100

    dotfiles

--- qmk/test/config.h
+++ qmk/test/config.h
@@ -35,4 +35,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 #define RETRO_TAPPING
+#define TAPPING_FORCE_HOLD
 
