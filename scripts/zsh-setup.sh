bbc1eb9324505d9b370ed6033937fa4e6f738496
malachi557 <thomasmalachi18@gmail>
2020-09-27 16:12:13 +0100

    changed zsh prompt to pure

--- scripts/zsh-setup.sh
+++ scripts/zsh-setup.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env 
 
-
+# install zsh and other #dependntsEs#
 sudo pacman -S zsh curl git || sudo apt-get update; sudo apt-get install zsh curl git
 
+# change default shell to zsh
 chsh -s $(which zsh)
 
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# install zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# install zsh pure prompt
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 
 
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,10 +1,23 @@
 
-ZSH_THEME="simple"
 plugins=(zsh-autosuggestions zsh-syntax-highlighting zsh-z)
-export ZSH=$HOME/.oh-my-zsh
 source $ZSH/oh-my-zsh.sh
 source ~/dotfiles/zsh/functions.zsh
 source ~/dotfiles/zsh/config.zsh
 source ~/dotfiles/zsh/aliases.zsh
 
 alias hat='echo "hats"'
+autoload -U promptinit; promptinit
+
+# optionally define some options
+PURE_CMD_MAX_EXEC_TIME=10
+
+# change the path color
+zstyle :prompt:pure:path color white
+
+# change the color for both `prompt:success` and `prompt:error`
+zstyle ':prompt:pure:prompt:*' color cyan
+
+# turn on git stash status
+zstyle :prompt:pure:git:stash show yes
+
+prompt pure
