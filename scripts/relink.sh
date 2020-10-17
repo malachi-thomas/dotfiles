6f72e1692594fc50d8ff6ff67595ec722597f4a7
banana_llarma <thomasmalachi18@gmail>
2020-10-17 12:08:22 +0100

    espanso

--- scripts/relink.sh
+++ scripts/relink.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # all os
-[ ! -d $HOME/.config/nvim ] && mkdir ~/.config/nvim
-[ ! -d $HOME/.config/nvim/lua ] && mkdir ~/.config/nvim/lua
-[ ! -d $HOME/.config/nvim/UltiSnips ] && mkdir ~/.config/nvim/UltiSnips
-[ ! -d $HOME/.config/.moc ] && mkdir ~/.config/.moc
+[ ! -d $HOME/.config/nvim/lua ] && mkdir -p ~/.config/nvim/lua
+[ ! -d $HOME/.config/nvim/UltiSnips ] && mkdir -p ~/.config/nvim/UltiSnips
+# [ ! -d $HOME/.config/.moc ] && mkdir ~/.config/.moc
 ln -f ~/dotfiles/nvim/* ~/.config/nvim
 ln -f ~/dotfiles/nvim/lua/* ~/.config/nvim/lua
 ln -f ~/dotfiles/nvim/UltiSnips/* ~/.config/nvim/UltiSnips
@@ -13,11 +12,11 @@ ln -f ~/dotfiles/zsh/.zshenv ~/.zshenv
 if [ ! -d /mnt/c ] ;then
 
   # if unix
-  [ ! -d $HOME/.config/i3 ] && mkdir ~/.config/i3
-  [ ! -d $HOME/.config/vifm ] && mkdir ~/.config/vifm
-  [ ! -d $HOME/.config/kitty ] && mkdir ~/.config/kitty
-  [ ! -d $HOME/.config/sxhkd ] && mkdir ~/.config/sxhkd
-  [ ! -d $HOME/.config/espanso ] && mkdir ~/.config/espanso
+  [ ! -d $HOME/.config/i3 ] && mkdir -p ~/.config/i3
+  [ ! -d $HOME/.config/vifm ] && mkdir -p ~/.config/vifm
+  [ ! -d $HOME/.config/kitty ] && mkdir -p ~/.config/kitty
+  [ ! -d $HOME/.config/sxhkd ] && mkdir -p ~/.config/sxhkd
+  [ ! -d $HOME/.config/espanso ] && mkdir -p ~/.config/espanso
   ln -f ~/dotfiles/espanso/default.yml ~/.config/espanso/default.yml
   ln -f ~/dotfiles/espanso/user/* ~/.config/espanso/user
   ln -f ~/dotfiles/i3/config ~/.config/i3/config
@@ -27,10 +26,9 @@ if [ ! -d /mnt/c ] ;then
 else
 
   # if windows
-  [ ! -d /mnt/c/User/malac/AppData/Roaming/espanso ] && mkdir /mnt/c/User/malac/AppData/Roaming/espanso
-  [ ! -d /mnt/c/User/malac/AppData/Roaming/espanso/user ] && mkdir /mnt/c/User/malac/AppData/Roaming/espanso/user
-  cp -f ~/dotfiles/espanso/default.yml /mnt/c/Users/malac/AppData/Roaming/espanso
-  cp -f ~/dotfiles/espanso/user/* /mnt/c/Users/malac/AppData/Roaming/espanso/user
+  [ ! -d /mnt/c/User/malac/AppData/Roaming/espanso/user ] && mkdir -p /mnt/c/User/malac/AppData/Roaming/espanso/user
+  ln -f ~/dotfiles/espanso/default.yml /mnt/c/Users/malac/AppData/Roaming/espanso
+  ln -f ~/dotfiles/espanso/user/* /mnt/c/Users/malac/AppData/Roaming/espanso/user
 fi
 
 
