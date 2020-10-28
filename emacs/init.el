9bca5f8102e7b08eeba45c85c06633322d195d67
banana_llarma <thomasmalachi18@gmail>
2020-10-28 12:09:37 +0000

    emacs added init.org

--- emacs/init.el
+++ emacs/init.el
@@ -24,12 +24,18 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t)
+  (setq doom-themes-enable-italic t)
+  (load-theme 'doom-gruvbox t))
+
 (use-package all-the-icons
   :ensure t
   :config
   (when (not (member "all-the-icons" (font-family-list)))
     (all-the-icons-install-fonts t)))
-   
+
 (use-package telephone-line
   :config
   (setq telephone-line-primary-right-separator 'telephone-line-abs-left
@@ -38,31 +44,101 @@
         telephone-line-evil-use-short-tag t)
   (telephone-line-mode 1))
 
-(use-package js2-mode)
-(use-package rjsx-mode)
-(use-package web-mode)
+(use-package dimmer
+  :config
+  (setq dimmer-adjustment-mode :foreground)
+  (setq dimmer-fraction 0.5)
+  (dimmer-mode t))
 
 (use-package evil
   :config
   (evil-mode 1)
-  (evil-set-initial-state 'help-mode 'normal))
+  (evil-set-initial-state 'help-mode 'normal)
+  (evil-set-initial-state 'info-mode 'normal))
 
-;; extra
-(use-package counsel
+(use-package evil-commentary
+  :config
+  (evil-commentary-mode))
+
+(use-package evil-multiedit
   :config
   (evil-define-key 'normal 'global
-    (kbd "SPC SPC") 'counsel-fzf
-    (kbd "SPC r f") 'counsel-recentf
-    (kbd "SPC c b") 'counsel-switch-buffer))
+    (kbd "C-n") 'evil-multiedit-match-and-next
+    (kbd "C-p") 'evil-multiedit-match-and-prev
+    (kbd "C-a") 'evil-multiedit-match-all))
 
-
-(use-package cl-lib)
-
-(use-package doom-themes
+(use-package evil-snipe
   :config
-  (setq doom-themes-enable-bold t)
-  (setq doom-themes-enable-italic t)
-  (load-theme 'doom-gruvbox t))
+  (evil-snipe-mode +1)
+  (evil-snipe-override-mode +1))
+
+(use-package undo-tree
+:config
+(global-undo-tree-mode 1))
+
+(use-package swiper
+:ensure t)
+
+(use-package org
+  :hook ((org-mode-hook . (lambda () (add-hook 'after-save-hook #'reload-config))))
+  :config
+  (setq org-ellipsis " ▾"))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(require 'org-tempo)
+(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(add-to-list 'org-structure-template-alist '("js" . "src javascipt"))
+(add-to-list 'org-structure-template-alist '("py" . "src python"))
+
+(use-package counsel)
+
+(use-package saveplace
+:config
+(save-place-mode))
+
+(use-package smart-hungry-delete
+  :ensure t
+  :defer nil
+  :config (smart-hungry-delete-add-default-hooks))
+
+(use-package avy
+  :config
+  (setq avy-keys '(?a ?o ?e ?u ?h ?t ?n ?s))
+  (setq avy-background t)
+  (setq avy-highlight-first t)
+  (setq avy-orders-alist
+        '((avy-goto-char . avy-order-closest)
+          (avy-goto-word-0 . avy-order-closest))))
+
+(use-package selectrum
+  :config
+  (selectrum-mode +1))
+
+(use-package selectrum-prescient
+  :config
+  (selectrum-prescient-mode))
+
+
+
+
+
+(use-package company
+  :config
+  (global-company-mode)
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1)
+  (setq company-selection-wrap-around t)
+  (company-tng-configure-default))
+
+(use-package company-prescient
+  :config
+  (company-prescient-mode))
 
 (use-package lsp-mode
   :ensure t
@@ -76,62 +152,16 @@
   :config
   (setq lsp-keymap-prefix "C-l"))
 
-(use-package company
-  :config
-  (global-company-mode)
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 1)
-  (setq company-selection-wrap-around t)
-  (company-tng-configure-default))
-
-(use-package saveplace
-  :config
-  (save-place-mode))
-
-(use-package evil-commentary
-  :config
-  (evil-commentary-mode))
-
-(use-package undo-tree
-  :config
-  (global-undo-tree-mode 1))
-
-;; (use-package ivy
-;;   :config
-;;   (setq ivy-use-virtual-buffers t))
-
-(use-package swiper
-:ensure t)
-
-;; (use-package ivy-prescient
-;;   :after counsel
-;;   :config
-;;   (ivy-prescient-mode))
-
-(use-package org
-  :config
-  (setq org-ellipsis " ▾"))
-
-(use-package org-bullets
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
-
-(use-package projectile
-  :config
-  (projectile-mode +1)
-  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
-
 (use-package emmet-mode
   :hook ((sgml-mode-hook . emmet-mode)
-         (css-mode-hook . emmet-mode)))
-:config
-(setq emmet-self-closing-tag-style "/")
-(setq emmet-expand-jsx-className? t)
-(setq emmet-move-cursor-between-quotes t)
+         (css-mode-hook . emmet-mode))
+  :config
+  (setq emmet-self-closing-tag-style "/")
+  (setq emmet-expand-jsx-className? t)
+  (setq emmet-move-cursor-between-quotes t))
+
+(use-package rainbow-delimiters
+  :ensure t)
 
 (use-package yasnippet
   :config
@@ -139,71 +169,13 @@
   (setq yas-snippet-dirs '("~/dotfiles/emacs/snippets"))
   (yas-reload-all))
 
-(use-package evil-multiedit
-  :config
-  (evil-define-key 'normal 'global
-    (kbd "C-n") 'evil-multiedit-match-and-next
-    (kbd "C-p") 'evil-multiedit-match-and-prev
-    (kbd "C-a") 'evil-multiedit-match-all))
-
-(use-package format-all
-  :config
-  (format-all-mode))
-
 (use-package flycheck
   :ensure t
   :init (global-flycheck-mode))
 
-(use-package doom-themes
+(use-package format-all
   :config
-  (setq doom-themes-enable-bold t)
-  (setq doom-themes-enable-italic t)
-  (load-theme 'doom-gruvbox t))
-
-(use-package avy
-  :config
-  (setq avy-keys '(?a ?o ?e ?u ?h ?t ?n ?s))
-  (setq avy-background t)
-  (setq avy-highlight-first t)
-  (setq avy-orders-alist
-        '((avy-goto-char . avy-order-closest)
-          (avy-goto-word-0 . avy-order-closest)))
-  (evil-define-key 'normal 'global
-    (kbd "C-l") 'ma/avy-goto-line
-    (kbd "C-f") 'ma/avy-goto-word-1))
-
-(use-package prescient)
-(use-package company-prescient
-  :config
-  (company-prescient-mode))
-(use-package selectrum-prescient
-  :config
-  (selectrum-prescient-mode))
-(use-package selectrum
-  :config
-  (selectrum-mode +1))
-(use-package evil-snipe
-  :config
-  (evil-snipe-mode +1)
-  (evil-snipe-override-mode +1))
-
-(use-package dimmer
-  :config
-  (setq dimmer-adjustment-mode :foreground)
-  (setq dimmer-fraction 0.5)
-  (dimmer-mode t))
-
-(use-package smart-hungry-delete
-  :config
-  (smart-hungry-delete-add-default-hooks)
-  (evil-define-key 'insert 'global
-    (kbd "<backspace>") 'smart-hungry-delete-backward-char))
-
-(require 'org-tempo)
-(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
-(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
-(add-to-list 'org-structure-template-alist '("js" . "src javascipt"))
-(add-to-list 'org-structure-template-alist '("py" . "src python"))
+  (format-all-mode))
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
@@ -212,7 +184,6 @@
  "C-s" 'save-buffer
  "C-w" 'delete-window
  ":" 'execute-extended-command
- "SPC" nil
  "<M-left>" 'evil-window-left
  "<M-down>" 'evil-window-down
  "<M-up>" 'evil-window-up
@@ -222,11 +193,14 @@
  "C-r" 'undo-tree-redo
  "n" 'evil-search-previous
  "N" 'evil-search-next
- "SPC s n" 'yas-new-snippet
+ "C-l" 'ma/avy-goto-line
+ "C-f" 'ma/avy-goto-word-1
+ "C-M-r" 'my/config-reload
  "/" 'swiper)
 
 (general-define-key
  :keymaps 'insert
+ "<backspace>" 'smart-hungry-delete-backward-char
  "C-s" 'emmet-expand-line
  "C-SPC" 'yas-maybe-expand
  "<up>" 'evil-previous-line
@@ -235,14 +209,14 @@
 (general-define-key
  :prefix "SPC"
  :keymaps 'normal
+ "SPC" 'counsel-fzf
+ "s n" 'yas-new-snippet
  "f f" 'find-file
  "h v" 'describe-variable
  "h k" 'describe-key
  "h m" 'describe-mode
- "r c" 'eval-buffer
+ "c d" 'cd
  "r f" 'counsel-recentf)
-
-(electric-pair-mode t)
 
 (setq inhibit-startup-message t)
 (setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
@@ -254,16 +228,18 @@
 (setq-default display-line-numbers-current-absolute t)
 (setq-default display-line-numbers-width 1)
 (setq-default display-line-numbers-widen t)
-
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
+
 (scroll-bar-mode -1) ; Disable visible scrollbar
 (tool-bar-mode -1) ; Disable the toolbar
 (tooltip-mode -1) ; Disable tooltips
 (set-fringe-mode 10) ; Give some breathing room
 (menu-bar-mode -1) ; Disable the menu bar
 (set-face-attribute 'default nil :font "Hack" :height 110)
-(show-paren-mode t)
+;; (show-paren-mode t)
+(electric-pair-mode t)
+(global-subword-mode 1)
 
 ;; org title font size
 (dolist (face '((org-level-1 . 1.4)
@@ -275,22 +251,15 @@
                 (org-level-7 . 1.1)
                 (org-level-8 . 1.05))))
 ;; org bulleted list start with a •
+
 (font-lock-add-keywords 'org-mode
                         '(("^ *\\([-]\\) "
                            (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
 (add-hook 'text-mode-hook #'display-line-numbers-mode)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
-;; (add-hook 'window-setup-hook 'dashboard-refresh-buffer)
 (add-hook 'before-save-hook 'format-all-buffer)
-
-;; (defun ma/org-babel-tangle-config ()
-  ;; (when (string-equal (buffer-file-name)
-                      ;; (expand-file-name "~/dotfiles/emacs/init.org"))
-    ;; (let ((org-confirm-babel-evaluate nil))
-      ;; (org-babel-tangle))))
-;; 
-(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'my/config-reload)))
 
 (evil-define-motion evil-next-line (count)
   "Move the cursor COUNT lines down."
@@ -320,13 +289,6 @@
     (evil-line-move (- (or count 1))))
   (recenter))
 
-(defun reload-config ()
-  (when (string-equal
-         (buffer-file-name)
-         (expand-file-name "~/dotfiles/emacs/init.org"))
-    (let ((org-confirm-babel-evaluate nil))
-      (org-babel-tangle))))
-
 (defun ma/avy-goto-char-2 (char1 char2 &optional arg beg end)
   (interactive (list (let ((c1 (read-char "char 1: " t)))
                        (if (memq c1 '(? ?\b))
@@ -354,7 +316,7 @@
      :end end))
   (recenter))
 
-(defun ma/avy-goto-word-1 (char &optional arg beg end symbol)
+(defun avy-goto-word-1 (char &optional arg beg end symbol)
   (interactive (list (read-char "char: " t)
                      current-prefix-arg))
   (avy-with avy-goto-word-1
@@ -374,7 +336,8 @@
                 :window-flip arg
                 :beg beg
                 :end end)))
-  (recenter))
+                (recenter))
+
 
 (defun ma/avy-goto-line ()
   (interactive)
@@ -390,9 +353,21 @@
 (use-package iedit)
 (use-package general)
 (use-package which-key
-  :ensure t
-  :init (which-key-mode))
-(which-key-setup-minibuffer)
-(setq which-key-side-window-max-height 0.25)
-(setq which-key-idle-delay 0.2)
-(setq which-key-prefix-prefix "+" )
+:ensure t
+:config
+(setq which-key-idle-delay 0.15)
+;; (setq which-key-idle-secondary-delay 0.05)
+(setq which-key-popup-type 'minibuffer)
+
+(which-key-mode))
+
+(use-package projectile
+:config
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+
+(use-package js2-mode)
+(use-package rjsx-mode)
+(use-package web-mode)
+(use-package cl-lib)
