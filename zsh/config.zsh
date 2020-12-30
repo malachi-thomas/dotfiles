b7db68ee604ccf9d5f7f37f4d3f5e06842bbcc2d
banana_llarma <thomasmalachi18@gmail>
2020-12-30 12:59:08 +0000

    added branch and

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -7,10 +7,22 @@ bindkey -s '^[' '' # Unmap esc
 bindkey -s '^n' '^[c' # bind alt+c to ctrl+n
 bindkey -v
 
+_git_repo_name() { 
+    gittopdir=$(git rev-parse --git-dir 2> /dev/null)
+    if [[ "foo$gittopdir" == "foo.git" ]]; then
+        echo " "`basename $(pwd)`":"
+    fi
+}
+_git_branch_name() {
+    git branch 2>/dev/null | awk '/^\*/ { print $2 }'
+}
+
 # Prompt
+setopt prompt_subst
 autoload -U colors && colors
 PS1='
-%F{magenta}%n@%m %F{blue}%~
+
+%F{magenta}%n@%m%f%F{cyan}$(_git_repo_name)$(_git_branch_name)%f %F{blue}%~
 %F{red}-%f '
 
 # Basic auto/tab complete
