fe268a28c9df5f2a132c99e94c7efe3318ef845a
banana-llarma <thomasmalachi18@gmail>
2021-05-06 09:11:51 +0100

    save

--- scripts/ian-script.sh
+++ scripts/ian-script.sh
@@ -2,23 +2,19 @@
 
 current_path=$HOME/test
 new_path=$HOME/test/hats
+delay_in_seconds=600
 
 # DONT CHANGE THIS
 command=$(ls -p $current_path | grep -v /)
 
-# loop forever
 while :; do
-  # if $current_path not empty
   if [[ ! -z $command ]]; then
-    # for loop on each thing in $current_path
     for thing in $current_path/*; do
       if [ -f $thing ]; then
-        # mv $thing to $new_path;
         mv $thing $new_path
         echo "moved at" `date +"%H:%M"`
       fi
     done
   fi
-  # delay 10 minuets
-  sleep 600
+  sleep $delay_in_seconds
 done
